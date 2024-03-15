641ad9c91ce7e52f6e4aacfdc3a8446dbbdf1dd1
Quest Henkart <qhenkart@gmail.com>
2024-03-15 13:19:14 +0800

    update imports

--- messages_test.go
+++ messages_test.go
@@ -7,8 +7,8 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/sashabaranov/go-openai"
-	"github.com/sashabaranov/go-openai/internal/test/checks"
+	"github.com/qhenkart/go-openai"
+	"github.com/qhenkart/go-openai/internal/test/checks"
 )
 
 var emptyStr = ""
